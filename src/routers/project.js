31d7dfd05a56f14a78338418ca87d64fe5f08d9e
andmak44 <andmak44@gmail.com>
2019-11-10 00:26:41 +0000

    add delete project route to api

--- src/routers/project.js
+++ src/routers/project.js
@@ -2,6 +2,7 @@ const express = require('express');
 const Project = require('../models/Project');
 const apiPath = process.env.API_PATH;
 const router = express.Router();
+const Transaction = require('mongoose-transactions');
 
 router.post(apiPath + '/project', async(req, res) => {
   //Create project
@@ -32,6 +33,24 @@ router.get(apiPath + '/project/task/:userId', async(req, res) => {
       res.send({ projects: projects });
   } catch (err) {
       res.status(401).json({ error: err.message });
+  }
+})
+
+router.delete(apiPath + '/project', async(req, res) => {
+  //Delete project and related tasks
+  const {projectId} = req.body;
+  const transaction = new Transaction();
+  try {
+    const todoListIds = await Project.findById(projectId, {'todoList':1});
+    todoListIds.todoList.forEach(id => {
+      transaction.remove('Task', id);
+    });
+    transaction.remove('Project', projectId);
+    await transaction.run();
+    res.status(200).send({ msg: `project with id ${projectId} removed and tasks: ${todoListIds.todoList}` });
+  } catch (err) {
+    await transaction.rollback().catch(console.error);
+    res.status(400).send(err);
   }
 })
 
